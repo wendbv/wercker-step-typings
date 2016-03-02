f33a4e69fa78f5148bd1c9204cbc2cdc4252da98
Sander Heling <sander@wend.nl>
2016-03-02 11:43:40 +0100

    Remove check if package is installed
    For now I’ll just remove it. We can add a better working check later on
    because this is not working.

--- run.sh
+++ run.sh
@@ -1,27 +1,8 @@
 #!/bin/bash
 
-# Function to detect if the package is installed
-function npm_package_is_installed {
-    if npm list --depth 1 "${1}" > /dev/null 2>&1; then
-        return 1
-    else
-        return 0
-    fi
-}
-
-# First make sure typings is installed
 if ! type typings &> /dev/null ; then
-    # Check if it is in repo
-    if npm_package_is_installed typings == 0 ; then
-        info "typings not installed, trying to install it through npm"
-
-        sudo npm install -g --silent typings
-        typings_command="typings"
-    else
-        info "typings is available locally"
-        debug "typings version: $(node ./node_modules/.bin/typings --version)"
-        typings_command="node ./node_modules/.bin/typings"
-    fi
+    sudo npm install -g --silent typings
+    typings_command="typings"
 else
     # typings is available globally
     info "typings is available"
